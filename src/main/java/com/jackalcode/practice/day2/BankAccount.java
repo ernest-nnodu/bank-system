e94f7b5eb4484695dd69ebb6d26a386aeead82c6
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-04 22:22:56 +0000

    Implement deposit in bank account

--- src/main/java/com/jackalcode/practice/day2/BankAccount.java
+++ src/main/java/com/jackalcode/practice/day2/BankAccount.java
@@ -17,22 +17,38 @@ public class BankAccount {
             throw new IllegalArgumentException("Account holder name is required");
         }
 
-        if (balance.intValue() < 0) {
-            throw new IllegalArgumentException("Balance should be zero or greater");
-        }
+        validateBalance(balance);
 
         this.accountNumber = accountNumber;
         this.accountHolderName = accountHolderName;
         this.balance = balance;
     }
 
+    private void validateBalance(BigDecimal balance) {
+        if (balance.intValue() < 0) {
+            throw new IllegalArgumentException("Balance should be zero or greater");
+        }
+    }
+
     public BigDecimal getBalance() {
         return this.balance;
     }
 
-    public void deposit(BigDecimal bigDecimal) {
+    public void deposit(BigDecimal amount) {
+        validateAmount(amount);
+        this.balance = this.balance.add(amount);
     }
 
     public void withdraw(BigDecimal bigDecimal) {
+    }
+
+    private void validateAmount(BigDecimal amount) {
+        if (amount == null) {
+            throw new IllegalArgumentException("Amount is required");
+        }
+
+        if (amount.intValue() <= 0) {
+            throw new IllegalArgumentException("amount should be greater than zero");
+        }
     }
 }
