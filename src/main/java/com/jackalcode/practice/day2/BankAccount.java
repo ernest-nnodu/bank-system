12ce9820f73c42779b6ef98a81debe6b416583cd
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-04 22:11:06 +0000

    Add bank account constructor

--- src/main/java/com/jackalcode/practice/day2/BankAccount.java
+++ src/main/java/com/jackalcode/practice/day2/BankAccount.java
@@ -1,4 +1,28 @@
 package com.jackalcode.practice.day2;
 
+import java.math.BigDecimal;
+
 public class BankAccount {
+
+    private final String accountNumber;
+    private String accountHolderName;
+    private BigDecimal balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, BigDecimal balance) {
+        if (accountNumber == null || accountNumber.isBlank()) {
+            throw new IllegalArgumentException("Account number is required");
+        }
+
+        if (accountHolderName == null || accountHolderName.isBlank()) {
+            throw new IllegalArgumentException("Account holder name is required");
+        }
+
+        if (balance.intValue() < 0) {
+            throw new IllegalArgumentException("Balance should be zero or greater");
+        }
+
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
 }
