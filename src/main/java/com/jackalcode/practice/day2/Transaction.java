30482c2faaf6febcc5dc9a39738bc72decc3af58
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-06 18:39:15 +0000

    Add transaction class

---
+++ src/main/java/com/jackalcode/practice/day2/Transaction.java
@@ -0,0 +1,66 @@
+package com.jackalcode.practice.day2;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Transaction {
+
+    private final String id;
+    private final String accountNumber;
+    private final TransactionType type;
+    private final BigDecimal amount;
+    private final Instant timestamp;
+
+    public Transaction(String accountNumber, TransactionType type, BigDecimal amount, Instant timestamp) {
+        this.id = UUID.randomUUID().toString();
+        this.accountNumber = accountNumber;
+        this.type = type;
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Transaction that = (Transaction) object;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id='" + id + '\'' +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", type=" + type +
+                ", amount=" + amount +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
