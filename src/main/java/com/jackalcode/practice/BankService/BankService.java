83f7754596378d18f86240372381a68fd8d8786d
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-08 10:25:46 +0000

    Implement create account in bank service

--- src/main/java/com/jackalcode/practice/BankService/BankService.java
+++ src/main/java/com/jackalcode/practice/BankService/BankService.java
@@ -1,4 +1,34 @@
 package com.jackalcode.practice.BankService;
 
+import com.jackalcode.practice.domain.BankAccount;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class BankService {
+
+    private final Map<String, BankAccount> accounts;
+
+    public BankService() {
+        this.accounts = new HashMap<>();
+    }
+
+    public BankAccount createAccount(String accountNumber, String accountHolderName, BigDecimal startingBalance) {
+
+        Objects.requireNonNull(accountNumber, "Account number is required");
+        Objects.requireNonNull(accountHolderName, "Account holder name is required");
+        Objects.requireNonNull(startingBalance, "Starting balance is required");
+
+        BankAccount newAccount = null;
+        try {
+            newAccount = new BankAccount(accountNumber, accountHolderName, startingBalance);
+            accounts.put(accountNumber, newAccount);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return newAccount;
+    }
 }
