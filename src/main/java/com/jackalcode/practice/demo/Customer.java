484764b2137769a82326b6ef0858c0d65c590360
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-14 22:05:45 +0000

    Refactor customer class to return total transaction performed

--- src/main/java/com/jackalcode/practice/demo/Customer.java
+++ src/main/java/com/jackalcode/practice/demo/Customer.java
@@ -4,11 +4,15 @@ import com.jackalcode.practice.BankService.BankService;
 
 import java.math.BigDecimal;
 import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.random.RandomGenerator;
 import java.util.stream.IntStream;
 
-public class Customer implements Runnable {
+public class Customer implements Callable<TotalTransaction> {
     private final String accountNumber;
     private final BankService bankService;
+    private TotalTransaction totalTransaction;
 
     public static Customer customerOf(String accountNumber, BankService bankService) {
         Objects.requireNonNull(accountNumber, "Bank account number is required");
@@ -26,10 +30,43 @@ public class Customer implements Runnable {
     }
 
     @Override
-    public void run() {
-        IntStream.rangeClosed(1, 10)
-                .forEach(amount -> bankService.deposit(accountNumber, BigDecimal.valueOf(amount * 10L)));
-        IntStream.rangeClosed(1, 10)
-                .forEach(amount -> bankService.withdraw(accountNumber, BigDecimal.valueOf(amount * 10L)));
+    public TotalTransaction call() {
+        RandomGenerator generator = RandomGenerator.getDefault();
+        BigDecimal totalDeposit = BigDecimal.ZERO;
+        BigDecimal totalWithdrawn = BigDecimal.ZERO;
+
+        for (int cycle = 0; cycle < 50; cycle++) {
+            int depositAmount = getAmount();
+            totalDeposit = totalDeposit.add(deposit(depositAmount));
+
+            int withdrawAmount = getAmount();
+            totalWithdrawn = totalWithdrawn.add(withdraw(withdrawAmount));
+        }
+
+        return new TotalTransaction(totalDeposit, totalWithdrawn);
+    }
+
+    private static int getAmount() {
+        RandomGenerator generator = RandomGenerator.getDefault();
+        return generator.nextInt(1, 101);
+    }
+
+    private BigDecimal deposit(int amount) {
+        BigDecimal deposit = bankService.deposit(accountNumber, BigDecimal.valueOf(amount));
+
+        if (deposit == null) {
+            return BigDecimal.ZERO;
+        }
+        return deposit;
+    }
+
+    private BigDecimal withdraw(int amount) {
+        BigDecimal withdraw = bankService.withdraw(accountNumber, BigDecimal.valueOf(amount));
+
+        if (withdraw == null) {
+            return BigDecimal.ZERO;
+        }
+
+        return withdraw;
     }
 }
