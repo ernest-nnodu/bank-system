300e9d41cf59f21d045f9ff58036e6bd05e3ad16
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-14 21:34:03 +0000

    Add total transaction record

--- src/main/java/com/jackalcode/practice/demo/TotalTransaction.java
+++ src/main/java/com/jackalcode/practice/demo/TotalTransaction.java
@@ -2,24 +2,8 @@ package com.jackalcode.practice.demo;
 
 import java.math.BigDecimal;
 
-public class TotalTransaction {
-
-    private BigDecimal totalDeposit;
-    private BigDecimal totalWithdraw;
-
-    public BigDecimal getTotalDeposit() {
-        return totalDeposit;
-    }
-
-    public void setTotalDeposit(BigDecimal totalDeposit) {
-        this.totalDeposit = totalDeposit;
-    }
-
-    public BigDecimal getTotalWithdraw() {
-        return totalWithdraw;
-    }
-
-    public void setTotalWithdraw(BigDecimal totalWithdraw) {
-        this.totalWithdraw = totalWithdraw;
-    }
+public record TotalTransaction(
+        BigDecimal totalDeposit,
+        BigDecimal totalWithdraw
+) {
 }
